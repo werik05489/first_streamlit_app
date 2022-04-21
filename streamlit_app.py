6f96813b48f95c6fc0d048dc3f1118882a8903ca
werik05489 <104116329+werik05489@users.noreply.github.com>
2022-04-20 21:55:56 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('My Mom\'s New Healthy Diner')
 
-streamlit.text('Breakfast Menu')
+streamlit.header('Breakfast Menu')
